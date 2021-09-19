f72add1a38434bd7b551f71ad63b44c886be5c6f
Miller <guf302010@gmail.com>
2021-09-19 20:03:24 +0700

    Initial Commit

---
+++ GuessApp/GuessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessAppApp.swift
+//  GuessApp
+//
+//  Created by Robert Miller on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
